4ddd02f1f71ee5048bf4bedebb3c7f287fe62a5f
Samuel Tremblay <samuel.tremblay65@gmail.com>
2023-05-30 22:37:32 -0400

    Final changes to shower screen. Created stats home page

---
+++ app/src/main/java/com/example/smartshower/StatisticsHome.java
@@ -0,0 +1,14 @@
+package com.example.smartshower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticsHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics_home);
+    }
+}
