24e219e147fe48448b4bf20b630bea2c52fad8c0
Samuel Tremblay <samuel.tremblay65@gmail.com>
2023-06-15 22:08:44 -0400

    Preset adapter now gets presets from database

--- app/src/main/java/com/example/smartshower/MainActivity.java
+++ app/src/main/java/com/example/smartshower/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.smartshower;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.view.View;
 import android.view.Window;
@@ -24,10 +23,15 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Local variables
+    ArrayList<UserPreset> userPresets;
+
     // Views
     RecyclerView presetListView;
     RecyclerView recommendedListView;
     Button btn_showStats;
+
+    PresetAdapter presetAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,37 +50,13 @@ public class MainActivity extends AppCompatActivity {
         // finally change the color
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.shower_blue300));
 
-        // Create the observer which updates the UI.
-        final Observer<List<UserPreset>> presetObserver = new Observer<List<UserPreset>>() {
-            @Override
-            public void onChanged(@Nullable final List<UserPreset> newPresets) {
-                Log.i("OBSERVER", "Detected change in database");
-            }
-        };
-
         // FIX ME: temporary preset list for development
         UserPreset preset1 = new UserPreset("Good morning", 25, 50, 100, 300, "cold");
-        UserPreset preset2 = new UserPreset("Good evening", 38, 60, 80, 120, "cold");
-        UserPreset preset3 = new UserPreset("Ice cold", 10, 25, 100, 300, "cold");
-        UserPreset preset4 = new UserPreset("Soothe", 38, 60, 100, 60, "cold");
-        UserPreset preset5 = new UserPreset("Kids", 35, 37, 90, 900, "cold");
-
-        ArrayList<UserPreset> presetList = new ArrayList<UserPreset>();
-        presetList.add(preset1);
-        presetList.add(preset2);
-        presetList.add(preset3);
-        presetList.add(preset4);
-        presetList.add(preset5);
 
         // Setting preset recycler view adapter and layout manager
         presetListView = (RecyclerView) findViewById(R.id.rv_home_presets);
         recommendedListView = (RecyclerView) findViewById(R.id.rv_home_recommended);
-        PresetAdapter presetAdapter = new PresetAdapter(presetList, new PresetClickListener() {
-            @Override
-            public void onItemClick(UserPreset preset) {
-                startPresetShower(preset);
-            }
-        });
+
 
         List<UserPreset> recommendedPresets = new ArrayList<>();
         recommendedPresets.add(preset1);
@@ -88,25 +68,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        presetListView.setAdapter(presetAdapter);
-        presetListView.setLayoutManager(new LinearLayoutManager(this));
-
         recommendedListView.setAdapter(recommendedAdapter);
         recommendedListView.setLayoutManager(new LinearLayoutManager(this));
 
         btn_showStats = findViewById(R.id.btn_home_viewStatistics);
 
+        loadUserPresets();
+
         btn_showStats.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveTask();
+
             }
         });
 
     }
 
-    public void startPresetShower(UserPreset preset)
-    {
+    public void startPresetShower(UserPreset preset) {
         Intent myIntent = new Intent(MainActivity.this, Shower.class);
         myIntent.putExtra("name", preset.name); //Optional parameters
         myIntent.putExtra("temperature", preset.temp); //Optional parameters
@@ -116,42 +94,37 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.startActivity(myIntent);
     }
 
+    public void updatePresets(List<UserPreset> presets) {
+        presetAdapter = new PresetAdapter(presets, this::startPresetShower);
+        presetListView.setAdapter(presetAdapter);
+        presetListView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
     // Database tasks
-    private void saveTask() {
-        class SaveTask extends AsyncTask<Void, Void, Void> {
+    private void loadUserPresets() {
+        @SuppressLint("StaticFieldLeak")
+        class LoadUserPresetsTask extends AsyncTask<Void, Void, Void> {
+
+            List<UserPreset> presets;
 
             @Override
             protected Void doInBackground(Void... voids) {
 
-                //creating a task
-                Task task = new Task();
-                task.setTask("New task");
-                task.setDesc("Random thing to do");
-                task.setFinishBy("Tomorrow");
-                task.setFinished(false);
-
-                // Creating presets
-                UserPreset preset1 = new UserPreset("Good morning", 25, 50, 100, 300, "cold");
-                UserPreset preset2 = new UserPreset("Good evening", 38, 60, 80, 120, "cold");
-                UserPreset preset3 = new UserPreset("Ice cold", 10, 25, 100, 300, "cold");
-                UserPreset preset4 = new UserPreset("Soothe", 38, 60, 100, 60, "cold");
-                UserPreset preset5 = new UserPreset("Kids", 35, 37, 90, 900, "cold");
-
                 // Adding to database
                 AppDatabase db = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase();
-                db.taskDao().insert(task);
-                db.userPresetDao().insertAll(preset1, preset2, preset3, preset4, preset5);
+                Log.i("JIRAF", "Hello");
+                presets = db.userPresetDao().getAll();
                 return null;
             }
 
             @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+            protected void onPostExecute(Void result) {
+                updatePresets(presets);
+                Toast.makeText(getApplicationContext(), "Got presets", Toast.LENGTH_LONG).show();
             }
         }
 
-        SaveTask st = new SaveTask();
-        st.execute();
+        LoadUserPresetsTask task = new LoadUserPresetsTask();
+        task.execute();
     }
 }
