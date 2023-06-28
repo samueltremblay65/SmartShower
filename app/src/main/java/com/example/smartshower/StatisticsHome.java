471feaf62ed67ea85ce35e59d2d9ebc5849e8e7b
Samuel Tremblay <samuel.tremblay65@gmail.com>
2023-06-27 22:22:23 -0400

    Fix: changed color of system bar on statistics activity

--- app/src/main/java/com/example/smartshower/StatisticsHome.java
+++ app/src/main/java/com/example/smartshower/StatisticsHome.java
@@ -1,9 +1,12 @@
 package com.example.smartshower;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.List;
@@ -16,6 +19,18 @@ public class StatisticsHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_home);
+
+        // Changing system bar color
+        Window window = this.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.shower_blue300));
 
         loadStatistics();
     }
