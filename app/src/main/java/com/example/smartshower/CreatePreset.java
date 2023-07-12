662fcef48983a9a0957e0749b98b9f14fb358491
Samuel Tremblay <samuel.tremblay65@gmail.com>
2023-07-12 12:25:01 -0400

    Saving state. Application crashing on entering shower screen

--- app/src/main/java/com/example/smartshower/CreatePreset.java
+++ app/src/main/java/com/example/smartshower/CreatePreset.java
@@ -3,23 +3,19 @@ package com.example.smartshower;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreatePreset extends AppCompatActivity {
-
-    // List of elements
-
-    // Labels
-    TextView nameLabel;
-    TextView temperatureLabel;
-    TextView flowrateLabel;
-    TextView themeLabel;
-    TextView temperatureLimitLabel;
-    TextView timerLabel;
 
     // TextEdits
     EditText nameInput;
@@ -29,7 +25,9 @@ public class CreatePreset extends AppCompatActivity {
     EditText temperatureLimitInput;
     EditText timerInput;
 
-    // Other input elements
+    // Buttons
+    Button createPreset;
+    Button discardChanges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +46,43 @@ public class CreatePreset extends AppCompatActivity {
         // finally change the color
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.shower_blue300));
 
+        nameInput = findViewById(R.id.et_preset_name);
+        temperatureInput = findViewById(R.id.et_preset_temperature);
+        flowrateInput = findViewById(R.id.et_preset_flow);
+
+        createPreset = findViewById(R.id.btn_create_preset);
+
+        createPreset.setOnClickListener(v -> {
+            int temperature = Integer.parseInt(temperatureInput.getText().toString());
+            int flowrate = Integer.parseInt(flowrateInput.getText().toString());
+            String presetName = nameInput.getText().toString().trim();
+
+            UserPreset preset = new UserPreset(presetName, temperature, flowrate, getResources().getInteger(R.integer.default_temperature_limit), getResources().getInteger(R.integer.null_timelimit_db_value), "default");
+
+            addPresetToDatabase(preset);
+
+            Intent myIntent = new Intent(CreatePreset.this, MainActivity.class);
+            CreatePreset.this.startActivity(myIntent);
+        });
     }
 
+    private void addPresetToDatabase(UserPreset preset) {
+        @SuppressLint("StaticFieldLeak")
+        class addPresetTask extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                AppDatabase db = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase();
+                db.userPresetDao().insertAll(preset);
+                return null;
+            }
 
+            @Override
+            protected void onPostExecute(Void result) {
+                Toast.makeText(CreatePreset.this, "Successfully created new preset", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        addPresetTask task = new addPresetTask();
+        task.execute();
+    }
 }
